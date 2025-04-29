103d66155a698f1eb341ea5748efb804728a0a1c
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-04-29 15:54:29 +0300

    'publish'

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import {CustomError} from '../errors/custom-error';
 
-export const errorHandler = (error: CustomError, req: Request, res: Response, next: NextFunction): void => {
+export const errorHandler = (error: unknown, req: Request, res: Response, next: NextFunction): void => {
     if(error instanceof CustomError){
         const { statusCode, message} = error;
         res.status(statusCode).json({message});
+        return;
     }
-    res.status(500).json({message: error.message || "Internal server Error!"});
+    const message = error instanceof Error ? error.message : 'Internal server Error!';
+    res.status(500).json({ message });
 }
